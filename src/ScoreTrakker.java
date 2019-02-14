70297345bc9736828a98074f03d66f32975a50a8
Luke Leonard <lukeleonard@mines.edu>
2019-02-14 15:15:49 -0700

    Added try/catch statements to handle exceptions

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -8,13 +8,14 @@ import java.util.Scanner;
 
 public class ScoreTrakker {
 	ArrayList<Student> students;
+	private String[] files = {"scores.txt", "badscore.txt", "nofile.txt" };
 	//Constructor that creates the ArrayList of Students
 	public ScoreTrakker() {
 		students  =new ArrayList<Student>();
 	}
 	
 	//Main that calls other functions
-	public static void main(String [ ] args) throws FileNotFoundException {
+	public static void main(String [ ] args){
 		ScoreTrakker tracker = new ScoreTrakker();
 		tracker.processFiles();
 	}
@@ -24,9 +25,20 @@ public class ScoreTrakker {
 		FileReader reader = new FileReader(name);
 		Scanner in = new Scanner(reader);
 		while(in.hasNextLine()) {
-			String readName = in.nextLine();
-			int readScore = Integer.valueOf(in.nextLine());
-			students.add(new Student(readName,readScore));
+			String readName = "";
+			String readScore = "";
+			int intScore = 0;
+			readName = in.nextLine();
+			readScore = in.nextLine();
+			try {
+			intScore = Integer.parseInt(readScore);
+			}
+			catch(NumberFormatException e){
+				System.out.println("Incorrect format for " + readName + " not a valid score: " + readScore);
+				System.out.println();
+				continue;
+			}
+			students.add(new Student(readName,intScore));
 		}
 	}
 	
@@ -37,12 +49,22 @@ public class ScoreTrakker {
 		for(Student student : students) {
 			System.out.println(student);
 		}
+		System.out.println();
 	}
 	
 	//Loads and Prints from a file
-	public void processFiles() throws FileNotFoundException {
-		loadDataFromFile("scores.txt");
-		printInOrder();
+	public void processFiles(){
+		for(String fileName : files) {
+			try {
+				loadDataFromFile(fileName);
+			} catch (FileNotFoundException e) {
+				System.out.println("Can't open file: " + fileName);
+				System.out.println();
+				continue;
+			}
+			printInOrder();
+			students.clear();
+		}
 	}
 
 }
