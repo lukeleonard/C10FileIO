c6599ad22d395437e4715e3a691767c4ff229984
Luke Leonard <lukeleonard@mines.edu>
2019-02-14 14:29:27 -0700

    Create student class

--- src/Student.java
+++ src/Student.java
@@ -1,8 +1,49 @@
+//Authors: Luke Leonard Casey Miller
 
-public class Student {
-
-	public Student() {
-		// TODO Auto-generated constructor stub
+public class Student implements Comparable<Student> {
+	private String name;
+	private int score;
+	
+	//Getters and Setters
+	public String getName() {
+		return name;
 	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	//Constructor
+	public Student(String name, int score) {
+		this.name = name;
+		this.score = score;
+		
+	}
+	//Comparing function using primative ints
+	@Override
+	public int compareTo(Student input) {
+		if (input.getScore() == this.score) {
+			return 0;
+		}else if(input.getScore() > this.score){
+			return -1;
+		}else {
+			return 1;
+		}
+	}
+
+	//to string
+	@Override
+	public String toString() {
+		return name + " " + score ;
+	}
+	
 
 }
